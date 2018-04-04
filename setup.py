529550904034e45838382025bc516816be7cf8d4
Eddie Hebert <ehebert@quantopian.com>
2018-04-04 02:36:54 +0000

    Test.

--- setup.py
+++ setup.py
@@ -7,39 +7,45 @@ except ImportError:
 
 from glob import glob
 from platform import system
-import numpy
 
 lib = []
 if system() == 'Linux':
     lib += ['rt']
 
-_ecos = Extension('_ecos', libraries = lib,
-                    # define LDL and AMD to use long ints
-                    # also define that we are building a python module
-                    define_macros = [
-                        ('PYTHON',None),
-                        ('DLONG', None),
-                        ('LDL_LONG', None),
-                        ('CTRLC', 1)],
-                    include_dirs = ['ecos/include', numpy.get_include(),
-                        'ecos/external/amd/include',
-                        'ecos/external/ldl/include',
-                        'ecos/external/SuiteSparse_config'],
-                    sources = ['src/ecosmodule.c',
-                        'ecos/external/ldl/src/ldl.c',
-                        'ecos/src/cone.c',
-                        'ecos/src/ctrlc.c',
-                        'ecos/src/ecos.c',
-                        'ecos/src/equil.c',
-                        'ecos/src/expcone.c',
-                        'ecos/src/kkt.c',
-                        'ecos/src/preproc.c',
-                        'ecos/src/spla.c',
-                        'ecos/src/splamm.c',
-                        'ecos/src/timer.c',
-                        'ecos/src/wright_omega.c'
-                    ] + glob('ecos/external/amd/src/*.c')
+
+def get_ext():
+    try:
+        import numpy
+    except ImportError:
+        return []
+    _ecos = Extension('_ecos', libraries = lib,
+                      # define LDL and AMD to use long ints
+                      # also define that we are building a python module
+                      define_macros = [
+                          ('PYTHON',None),
+                          ('DLONG', None),
+                          ('LDL_LONG', None),
+                          ('CTRLC', 1)],
+                      include_dirs = ['ecos/include', numpy.get_include(),
+                                      'ecos/external/amd/include',
+                                      'ecos/external/ldl/include',
+                                      'ecos/external/SuiteSparse_config'],
+                      sources = ['src/ecosmodule.c',
+                                 'ecos/external/ldl/src/ldl.c',
+                                 'ecos/src/cone.c',
+                                 'ecos/src/ctrlc.c',
+                                 'ecos/src/ecos.c',
+                                 'ecos/src/equil.c',
+                                 'ecos/src/expcone.c',
+                                 'ecos/src/kkt.c',
+                                 'ecos/src/preproc.c',
+                                 'ecos/src/spla.c',
+                                 'ecos/src/splamm.c',
+                                 'ecos/src/timer.c',
+                                 'ecos/src/wright_omega.c'
+                      ] + glob('ecos/external/amd/src/*.c')
                       + glob('ecos/ecos_bb/*.c'))       # glob bb source files
+    return _ecos
 
 setup(
     name = 'ecos',
@@ -52,7 +58,7 @@ setup(
     license = "GPLv3",
     package_dir = {'': 'src'},
     py_modules = ['ecos'],
-    ext_modules = [_ecos],
+    ext_modules = [get_ext()],
     install_requires = [
         "numpy >= 1.6",
         "scipy >= 0.9"
